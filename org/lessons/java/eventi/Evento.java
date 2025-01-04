2aab6617200f65767ab5f042694d73001e8baecc
davideblaffard <infodavide98@gmail.com>
2025-01-04 15:31:57 +0100

    add: added attributes and constructor method

--- org/lessons/java/eventi/Evento.java
+++ org/lessons/java/eventi/Evento.java
@@ -1,5 +1,24 @@
 package org.lessons.java.eventi;
 
+import java.time.LocalDate;
+
 public class Evento {
-    
+    private String titolo;
+    private LocalDate data;
+    private final int postiTotali;
+    private int postiPrenotati;
+
+    public Evento(String titolo, LocalDate data, int postiTotali) {
+        if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("La data dell'evento non può essere nel passato.");
+        }
+        if (postiTotali <= 0) {
+            throw new IllegalArgumentException("Il numero di posti totali deve essere positivo.");
+        }
+
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
 }
