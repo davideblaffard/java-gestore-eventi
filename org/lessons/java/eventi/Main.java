75313ba2e26725be2ecaa6f7d331bd8db5bc262d
davideblaffard <infodavide98@gmail.com>
2025-01-13 11:23:34 +0100

    feat: new Evento

--- org/lessons/java/eventi/Main.java
+++ org/lessons/java/eventi/Main.java
@@ -1,10 +1,26 @@
 package org.lessons.java.eventi;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args){
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Inserisci il titolo dell'evento");
+        System.out.println("Inserire il titolo dell'evento: ");
+        String titolo = scanner.nextLine();
+
+        System.out.println("Inserire la data dell'evento(formato yyyy-mm-dd): ");
+        String dataInput = scanner.nextLine();
+        LocalDate data = LocalDate.parse(dataInput);
+
+        System.out.println("Inserire il numero totale di posti: ");
+        int postiTotali = scanner.nextInt();
+
+        Evento evento = new Evento(titolo, data, postiTotali);
+        System.out.println("Hai creato l'evento: " + evento);
+
+
+
+        scanner.close();
     }
 }
