a26b0802ad74e72e42618d3747c5ce16840d7326
davideblaffard <infodavide98@gmail.com>
2025-01-13 11:45:07 +0100

    feat: added switch case

--- org/lessons/java/eventi/Main.java
+++ org/lessons/java/eventi/Main.java
@@ -19,7 +19,43 @@ public class Main {
         Evento evento = new Evento(titolo, data, postiTotali);
         System.out.println("Hai creato l'evento: " + evento);
 
+        boolean avanti = true;
+        while(avanti) {
+            System.out.println("\n ***** MENU *****");
+            System.out.println("1. Prenota un posto");
+            System.out.println("2. Disdici prenotazione");
+            System.out.println("3. Mostra info evento");
+            System.out.println("4. ESCI");
 
+            System.out.println("\n Scegli un opzione: ");
+            int scelta = scanner.nextInt();
+
+        switch (scelta) {
+            case 1:
+                System.out.println(evento.prenota());
+                break;
+            case 2:
+                System.out.println(evento.disdici());
+                break;
+
+            case 3: 
+                System.out.println("EVENTO: " + evento);
+                System.out.println("I posti totali per questo evento sono: " + evento.getPostiTotali());
+                System.out.println("I posti già prenotati per questo evento sono: " + evento.getPostiPrenotati());
+                int postiRimanenti = evento.getPostiTotali() - evento.getPostiPrenotati();
+                System.out.println("Per questo evento sono ancora disponibili " + postiRimanenti + " posti!");
+                break;
+            
+            case 4:
+                System.out.println("Arrivederci!");
+                avanti = false;
+                break;
+
+            default:
+                System.out.println("Scelta non valida. Inserisci un numero compreso tra 1 e 4");
+                break;
+        }
+        }
 
         scanner.close();
     }
