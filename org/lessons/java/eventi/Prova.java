46957f4ceee7fb39663dd36a7e9eb95bc5552a00
davideblaffard <infodavide98@gmail.com>
2025-01-21 11:00:35 +0100

    add: created Prova class

---
+++ org/lessons/java/eventi/Prova.java
@@ -0,0 +1,10 @@
+package org.lessons.java.eventi;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+public class Prova {
+
+}
